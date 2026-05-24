2b6e452612651b7e06419ae515a3cfb23e735ed7
JaOnwude <ojamesuchenna61@gmail.com>
2026-05-24 08:19:38 +0100

    refactored server.js for cors issues

--- server.js
+++ server.js
@@ -1,17 +1,52 @@
 
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');          
+// const mongodb = require('./db/connect');
+
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// // Enable CORS 
+// app.use(cors({
+//   origin: '*',                   
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true
+// }));
+
+// app.use(bodyParser.json());
+
+// app.use('/', require('./routes'));
+
+// // Global error handler
+// process.on('uncaughtException', (err, origin) => {
+//   console.error(`Caught exception: ${err}\nException origin: ${origin}`);
+// });
+
+// mongodb.initDb((err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     app.listen(port, () => {
+//       console.log(`Connected to DB and listening on ${port}`);
+//     });
+//   }
+// });
+
 const express = require('express');
 const bodyParser = require('body-parser');
-const cors = require('cors');          
+const cors = require('cors');
 const mongodb = require('./db/connect');
 
 const port = process.env.PORT || 8080;
 const app = express();
 
-// Enable CORS 
+
 app.use(cors({
   origin: '*',                   
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
   credentials: true
 }));
 
@@ -19,9 +54,16 @@ app.use(bodyParser.json());
 
 app.use('/', require('./routes'));
 
-// Global error handler
-process.on('uncaughtException', (err, origin) => {
-  console.error(`Caught exception: ${err}\nException origin: ${origin}`);
+// Root welcome route
+app.get('/', (req, res) => {
+  res.json({
+    message: "Personal Library API is running successfully!",
+    docs: "/api-docs"
+  });
+});
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err);
 });
 
 mongodb.initDb((err) => {
