3282a107b5dea469aae6978eeee6f056a265bbd5
JaOnwude <ojamesuchenna61@gmail.com>
2026-05-30 00:34:59 +0100

    Updated Swagger with OAuth support

--- swagger.js
+++ swagger.js
@@ -1,12 +1,40 @@
+// const swaggerAutogen = require('swagger-autogen')();
+
+// const doc = {
+//   info: {
+//     title: 'Personal Library API',
+//     description: 'CSE341 - My Personal Library Management API'
+//   },
+//   host: 'cse341-block3-weeks03-04.onrender.com',
+//   schemes: ['https']
+// };
+
+// const outputFile = './swagger.json';
+// const endpointsFiles = ['./routes/index.js'];
+
+// swaggerAutogen(outputFile, endpointsFiles, doc);
+
 const swaggerAutogen = require('swagger-autogen')();
 
 const doc = {
   info: {
     title: 'Personal Library API',
-    description: 'CSE341 - My Personal Library Management API'
+    description: 'CSE341 - My Personal Library API with Google OAuth',
+    version: '2.0.0'
   },
   host: 'cse341-block3-weeks03-04.onrender.com',
-  schemes: ['https']
+  schemes: ['https'],
+  securityDefinitions: {
+    googleOAuth: {
+      type: 'oauth2',
+      flow: 'implicit',
+      authorizationUrl: 'https://accounts.google.com/o/oauth2/auth',
+      scopes: {
+        'profile': 'Access your basic profile',
+        'email': 'Access your email'
+      }
+    }
+  }
 };
 
 const outputFile = './swagger.json';
